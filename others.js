1e49bb4876e10180dd4385fcb183086bc4f30a16
YestoryFornever <guosj90@163.com>
2016-07-09 20:17:37 +0800

    javascript.js start
    html>jscode>javascript>css>browser>es6>lib>others

--- others.js
+++ others.js
@@ -181,14 +181,14 @@ JavaScript 预编译问题
 			* http状态码有那些？分别代表是什么意思？
 			简单版
 			[
-				100Continue 继续，一般在发送post请求时，已发送了http header之后服务端将返回此信息，表示确认，之后发送具体参数信息
-				200OK 正常返回信息
-				201Created请求成功并且服务器创建了新的资源
-				202Accepted 服务器已接受请求，但尚未处理
-				301Moved Permanently请求的网页已永久移动到新位置。
+				100 Continue 继续，一般在发送post请求时，已发送了http header之后服务端将返回此信息，表示确认，之后发送具体参数信息
+				200 OK 正常返回信息
+				201 Created请求成功并且服务器创建了新的资源
+				202 Accepted 服务器已接受请求，但尚未处理
+				301 Moved Permanently请求的网页已永久移动到新位置。
 				302 Found 临时性重定向。
 				303 See Other 临时性重定向，且总是使用 GET 请求新的 URI。
-				304Not Modified 自从上次请求后，请求的网页未修改过。
+				304 Not Modified 自从上次请求后，请求的网页未修改过。
 
 				400 Bad Request服务器无法理解请求的格式，客户端不应当尝试再次使用相同的内容发起请求。
 				401 Unauthorized 请求未授权。
