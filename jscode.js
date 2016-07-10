f10d18f56a857101365627bfe3372de8b4df0141
YestoryFornever <guosj90@163.com>
2016-07-10 18:23:49 +0800

    add 54th MVC

--- jscode.js
+++ jscode.js
@@ -1041,3 +1041,42 @@
 		xxx.LEGS_NUM;
 		xxx.walking();
 		xxx.learn('math');
+54.		"Hello World" in MVC
+
+		var M = {}, V = {}, C = {};
+		M.data = "hello world";//米
+		V.render = function (M) { alert(M.data); }//为炊
+		C.handleOnload = function () { V.render(M); }//饭点
+		window.onload = C.handleOnLoad;
+
+		var M = {}, V = {}, C = {};
+		/* Model View Controller Pattern with Form Example */
+		/* Controller Handles the Events */
+		M = {
+			data: {
+				userName : "Dummy Guy",
+				userNumber : "000000000"
+			}, 
+			setData : function(d){
+				this.data.userName = d.userName;
+				this.data.userNumber = d.userNumber;
+			},
+			getData : function(){
+				return data;
+			}
+		};
+		V = {
+			userName : document.querySelector("#inputUserName"),
+			userNumber : document.querySelector("#inputUserNumber"),
+			update: function(M){
+				this.userName.value = M.data.userName;
+				this.userNumber.value = M.data.userNumber;
+			}
+		};
+		C = {
+			model: M,
+			view: V,
+			handler: function(){
+				this.view.update(this.model);
+			}
+		};
